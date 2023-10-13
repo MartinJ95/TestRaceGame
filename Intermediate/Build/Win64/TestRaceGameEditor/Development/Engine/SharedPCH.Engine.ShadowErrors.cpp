81147af7b2f8efad620030581201380deb76da4a
MartinJ95 <martinjones95@gmx.co.uk>
2023-10-13 20:03:51 +0100

    intitial commit
    this commit should serve as well.. the beginning commit officially birthing the project

---
+++ Intermediate/Build/Win64/TestRaceGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/marti/Documents/GitHub/RacingGame/TestRaceGame/Intermediate/Build/Win64/TestRaceGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/TestRaceGame/TestRaceGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestRaceGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestRaceGame, "TestRaceGame" );
