b74a016c14d78fb14482222c426647d23d8b0c31
MartinJ95 <martinjones95@gmx.co.uk>
2023-10-19 00:40:05 +0100

    feat editorFuntion : implementation
    this commit should allow the initial imlplementation of the editor function utility struct.

--- Source/TestRaceGame/RacingTrack.h
+++ Source/TestRaceGame/RacingTrack.h
@@ -7,6 +7,31 @@
 #include "RaceTrackSegment.h"
 #include "RacingTrack.generated.h"
 
+USTRUCT()
+struct FEditorFunction
+{
+	GENERATED_BODY()
+public:
+	FEditorFunction() : m_shouldRun(false), m_function([](void* ptr) {}), ownerPtr(nullptr) {}
+	FEditorFunction(const std::function<void(void*)>& function, void* owner) : m_shouldRun(false), m_function(function), ownerPtr(owner) {}
+	void CheckRunFunction()
+	{
+		if (m_shouldRun)
+		{
+			m_function(ownerPtr);
+			m_shouldRun = false;
+		}
+	}
+protected:
+	UPROPERTY(EditAnywhere)
+		bool m_shouldRun;
+	UPROPERTY()
+		std::function<void(void*)> m_function = [](void* ptr) {};
+	UPROPERTY()
+		void* ownerPtr;
+};
+
+
 UCLASS()
 class TESTRACEGAME_API ARacingTrack : public AActor
 {
@@ -19,6 +44,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere)
+	TArray<ARaceTrackSegment*> m_segments;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
