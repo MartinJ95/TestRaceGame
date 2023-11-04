2d5e5906b5d8a6816aadbbbe8acd5f23482ae9f0
MartinJ95 <martinjones95@gmx.co.uk>
2023-11-04 19:51:52 +0000

    feat add rawmesh..

--- Source/TestRaceGame/TestRaceGame.Build.cs
+++ Source/TestRaceGame/TestRaceGame.Build.cs
@@ -10,7 +10,7 @@ public class TestRaceGame : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "RawMesh" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
